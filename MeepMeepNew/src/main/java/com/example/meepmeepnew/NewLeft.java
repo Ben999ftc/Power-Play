e6668a8290c165465c6ef230663881b01599a5c7
Ben Loewen <bloewen999@gmail.com>
2023-03-11 13:39:42 -0700

    New MeepMeep path

---
+++ MeepMeepNew/src/main/java/com/example/meepmeepnew/NewLeft.java
@@ -0,0 +1,36 @@
+package com.example.meepmeepnew;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class NewLeft {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(31.5, 63, Math.toRadians(0)))
+                                .splineToConstantHeading(new Vector2d(36, 50), Math.toRadians(270))
+                                .splineToConstantHeading(new Vector2d(36, 25), Math.toRadians(270))
+                                .splineToSplineHeading(new Pose2d(27, 4.3, Math.toRadians(45)), Math.toRadians(225))
+                                .waitSeconds(0.2)
+                                .splineToSplineHeading(new Pose2d(40, 11.5, Math.toRadians(0)), Math.toRadians(0))
+                                .splineToConstantHeading(new Vector2d(63, 11.5), Math.toRadians(0))
+                                .waitSeconds(0.2)
+                                .lineToConstantHeading(new Vector2d(40, 9))
+                                .splineToSplineHeading(new Pose2d(27, 4.3, Math.toRadians(20)), Math.toRadians(200))
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
