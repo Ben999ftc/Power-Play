7147ccc6ce8e97efcff5cf425f02530ff329e30c
luker :) <lukermulholland@gmail.com>
2023-03-11 14:38:31 -0700

    sdfghjkl

--- MeepMeepNew/src/main/java/com/example/meepmeepnew/LeftBlue.java
+++ MeepMeepNew/src/main/java/com/example/meepmeepnew/LeftBlue.java
@@ -14,16 +14,19 @@ public class LeftBlue {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(31, 63, Math.toRadians(270)))
-                                .splineToConstantHeading(new Vector2d(36, 48), Math.toRadians(270))
-                                .splineToConstantHeading(new Vector2d(36, 0), Math.toRadians(270))
-                                .splineToLinearHeading(new Pose2d(30, 3.4, Math.toRadians(45)), Math.toRadians(225))
-                                .splineToSplineHeading(new Pose2d(36, 11.5, Math.toRadians(0)), Math.toRadians(0))
-                                .splineToConstantHeading(new Vector2d(65, 11.5), Math.toRadians(0))
-                                .splineToConstantHeading(new Vector2d(40, 11.5), Math.toRadians(180))
-                                .splineTo(new Vector2d(30, 3.4), Math.toRadians(225))
+                        drive.trajectorySequenceBuilder(new Pose2d(31.5, 63, Math.toRadians(0)))
+                                .splineToConstantHeading(new Vector2d(36, 50), Math.toRadians(270))
+                                .splineToConstantHeading(new Vector2d(36, 25), Math.toRadians(270))
+                                .splineToSplineHeading(new Pose2d(27, 4.3, Math.toRadians(45)), Math.toRadians(225))
+                                .waitSeconds(0.2)
+                                .splineToSplineHeading(new Pose2d(40, 11.5, Math.toRadians(0)), Math.toRadians(0))
+                                .splineToConstantHeading(new Vector2d(63, 11.5), Math.toRadians(0))
+                                .waitSeconds(0.2)
+                                .lineToConstantHeading(new Vector2d(40, 9))
+                                .splineToSplineHeading(new Pose2d(27, 4.3, Math.toRadians(20)), Math.toRadians(200))
                                 .build()
                 );
+
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
--- MeepMeepNew/src/main/java/com/example/meepmeepnew/RightBlue.java
+++ MeepMeepNew/src/main/java/com/example/meepmeepnew/RightBlue.java
@@ -14,33 +14,16 @@ public class RightBlue {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-41, 65, Math.toRadians(270)))
-                                // Claw grips
-                                .splineToConstantHeading(new Vector2d(-36, 50), Math.toRadians(270))
-                                .splineToConstantHeading(new Vector2d(-36, 12), Math.toRadians(270))
-                                .turn(Math.toRadians(-135))
-                                // Raise slides, raise arm, lower V
-                                .back(8)
-                                // Claw releases
-                                .forward(8)
-                                // Lower slides, lower arm, raise V
-                                .turn(Math.toRadians(45))
-                                // Repeat following trajectory
-                                // Raise slides to correct height, put arm out slightly
-                                .splineToConstantHeading(new Vector2d(-65, 12), Math.toRadians(180))
-                                // Claw grips, slides raise
-                                .splineToConstantHeading(new Vector2d(-36, 12), Math.toRadians(0))
-                                .turn(Math.toRadians(-45))
-                                // Raise slides, raise arm, lower V
-                                .back(8)
-                                // Claw releases
-                                .forward(8)
-                                // Lower slides, lower arm, raise V
-                                .turn(Math.toRadians(45))
-                                // Move to randomized position
-                                .splineToConstantHeading(new Vector2d(-60, 12), Math.toRadians(180)) // Right, Position 3
-                                .splineToConstantHeading(new Vector2d(-12, 12), Math.toRadians(0)) // Left, Position 1
-                                // Middle case stays put
+                        drive.trajectorySequenceBuilder(new Pose2d(-31.5, 63, Math.toRadians(180)))
+                                .splineToConstantHeading(new Vector2d(-36, 50), Math.toRadians(630))
+                                .splineToConstantHeading(new Vector2d(-36, 25), Math.toRadians(630))
+                                .splineToSplineHeading(new Pose2d(-31, 8, Math.toRadians(135)), Math.toRadians(325))
+                                .waitSeconds(0.2)
+                                .splineToSplineHeading(new Pose2d(-40, 11.5, Math.toRadians(180)), Math.toRadians(180))
+                                .splineToConstantHeading(new Vector2d(-63, 11.5), Math.toRadians(180))
+                                .waitSeconds(0.2)
+                                .lineToConstantHeading(new Vector2d(-62.5, 11.5))
+                                .splineToSplineHeading(new Pose2d(-31, 8, Math.toRadians(135)), Math.toRadians(325))
                                 .build()
                 );
 
